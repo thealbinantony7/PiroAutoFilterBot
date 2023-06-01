fcc88a053070405dfb732cd481183ca46abf2053
thealbinantony7 <131271238+thealbinantony7@users.noreply.github.com>
2023-06-01 16:21:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,11 +1,11 @@
-import os 
- import logging 
- import random 
- import asyncio 
- from Script import script 
- from pyrogram import Client, filters, enums 
- from pyrogram.errors import ChatAdminRequired, FloodWait 
- from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
+import os
+import logging
+import random
+import asyncio
+from Script import script
+from pyrogram import Client, filters, enums
+from pyrogram.errors import ChatAdminRequired, FloodWait 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
  from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, get_bad_files 
  from database.users_chats_db import db 
  from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, SUPPORT_CHAT, PROTECT_CONTENT, REQST_CHANNEL, SUPPORT_CHAT_ID, MAX_B_TN 
